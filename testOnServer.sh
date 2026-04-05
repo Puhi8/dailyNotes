50a3c75f94b6154398ee2689ee3516a19070f3b9
puhi8 <kruhek000@gmail.com>
2026-04-05 21:06:57 +0200

    delete testOnServer

--- testOnServer.sh
+++
@@ -1,3 +0,0 @@
-docker build -t dailynotestest:latest . && \
-docker save dailynotestest:latest | ssh -i /home/luka/.ssh/docker root@dockergw.homelab.puhek.si 'docker load' && \
-ssh -i /home/luka/.ssh/docker root@dockergw.homelab.puhek.si 'cd /home/docker/dailyNotesTesting && docker compose up -d --force-recreate'
