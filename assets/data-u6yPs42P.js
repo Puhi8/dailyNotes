38e1c26bd307397ab6d93f8dc9a4b0bb06306537
puhi8 <kruhek000@gmail.com>
2026-04-15 21:02:44 +0200

    Updates

---
+++ assets/data-u6yPs42P.js
@@ -0,0 +1,1 @@
+const e={short:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],long:["January","February","March","April","May","June","July","August","September","October","November","December"]},r={short:["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]};export{e as m,r as w};
